abd1f44f38a2e34e4309fa76bd015d8f828080fe
NEETHUPRIYA-7 <ramireddyneethu@gmail.com>
2026-05-02 12:02:41 +0530

    fixed port isuue

--- backend/server.js
+++ backend/server.js
@@ -620,9 +620,9 @@ app.post("/api/marks/reject", async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-
 
